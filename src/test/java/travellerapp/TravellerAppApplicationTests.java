6423bd38fee8a36c8ea639dadcbb0c1a088e5bf8
floppi19 <florin.avasiloaie@gmail.com>
2023-03-13 18:17:31 +0200

    init commit

---
+++ src/test/java/travellerapp/TravellerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package travellerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
